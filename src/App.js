e22c8094372066cf96289f28ab408cb265cb55f2
Vijayaranga <112083022+Vijayaranga@users.noreply.github.com>
2023-02-28 19:11:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with javascript
         </a>
       </header>
     </div>
